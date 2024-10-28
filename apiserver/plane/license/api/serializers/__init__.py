80907af264100e54a48abfa9822285be67d8483e
xavierspacelix <juanakbar361@gmail.com>
2024-10-28 19:20:23 +0700

    Separate Server And Frontend

--- apiserver/plane/license/api/serializers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .instance import (
-    InstanceSerializer,
-)
-
-from .configuration import InstanceConfigurationSerializer
-from .admin import InstanceAdminSerializer, InstanceAdminMeSerializer
